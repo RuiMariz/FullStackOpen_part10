b717575769e0a0b778b5dc4f8195fecb4dda6dc1
Rui Mariz <ruivmariz@gmail.com>
2021-06-15 22:43:18 +0100

    Complete 10.6

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { View, StyleSheet } from 'react-native';
+import { Link } from "react-router-native";
 import Constants from 'expo-constants';
 import theme from '../theme';
 import AppBarTab from './AppBarTab';
@@ -8,16 +9,18 @@ const styles = StyleSheet.create({
   container: {
     paddingTop: Constants.statusBarHeight,
     backgroundColor: theme.colors.appBarBackground,
+    flexDirection: 'row',
   },
   text: {
     color: theme.colors.appBarText,
-  }
+  },
 });
 
 const AppBar = () => {
   return (
     <View style={styles.container}>
-      <AppBarTab text='Repositories' />
+      <AppBarTab text='Repositories' link={'/'} />
+      <AppBarTab text='SignIn' link={'/sign-in'}/>
     </View>
   );
 };
--- src/components/AppBarTab.jsx
+++ src/components/AppBarTab.jsx
@@ -1,19 +1,23 @@
 import React from 'react';
 import { StyleSheet, Pressable } from 'react-native';
+import { Link } from "react-router-native";
 import Text from './Text';
 import theme from '../theme';
 
 const styles = StyleSheet.create({
   text: {
     color: theme.colors.appBarText,
+  },
+  container: {
+    marginRight: 10,
   }
 });
 
-const AppBarTab = ({ text }) => {
+const AppBarTab = ({ text, link }) => {
   return (
-    <Pressable>
+    <Link to={link} component={Pressable} style={styles.container}>
       <Text style={styles.text}>{text}</Text>
-    </Pressable>
+    </Link>
   );
 };
 
