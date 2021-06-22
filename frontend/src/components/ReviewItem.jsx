765be43912c7bd2f7272c13e3a858d75a5a1d5d5
Rui Mariz <ruivmariz@gmail.com>
2021-06-22 12:36:28 +0100

    Complete 10.20

---
+++ frontend/src/components/ReviewItem.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+import { format,parseISO } from 'date-fns';
+import theme from '../theme';
+
+const styles = StyleSheet.create({
+  reviewContainer: {
+    flexDirection: 'row',
+    marginBottom:10
+  },
+  ratingContainer: {
+    borderColor: theme.colors.primary,
+    borderWidth: 3,
+    width: 60,
+    height: 60,
+    justifyContent: 'center',
+    borderRadius: 60 / 2,
+    margin: 10,
+    marginTop: 0
+  },
+  ratingText: {
+    color: theme.colors.primary,
+    fontWeight: theme.fontWeights.bold,
+    fontSize: theme.fontSizes.subheading,
+    alignSelf: 'center',
+  },
+  name: {
+    fontWeight: theme.fontWeights.bold
+  },
+  reviewInfoContainer: {
+    flex: 1,
+    marginRight: 10
+  },
+  reviewTextContainer:{
+    marginTop:5
+  }
+});
+
+const Review = ({ review }) => {
+  return (
+    <View style={styles.reviewContainer}>
+      <View style={styles.ratingContainer}>
+        <Text style={styles.ratingText}>
+          {review.rating}
+        </Text>
+      </View>
+      <View style={styles.reviewInfoContainer}>
+        <Text style={styles.name} >{review.user.username}</Text>
+        <Text style={styles.review}>{format(parseISO(review.createdAt), 'dd/MM/yyyy')}</Text>
+        <View style={styles.reviewTextContainer}>
+          <Text>{review.text}</Text>
+        </View>
+      </View>
+    </View>
+  );
+};
+
+export default Review;
--- frontend/src/components/SingleRepository.jsx
+++ frontend/src/components/SingleRepository.jsx
@@ -1,34 +1,54 @@
 import React from 'react';
 import { useParams } from "react-router-native";
-import { View, Button, StyleSheet, Linking } from 'react-native';
+import { View, Button, StyleSheet, Linking, FlatList } from 'react-native';
 import useRepository from '../hooks/useRepository';
 import RepositoryItem from './RepositoryItem';
+import Review from './ReviewItem';
 import theme from '../theme';
 
 const styles = StyleSheet.create({
   buttonContainer: {
     margin: 20
-  }
+  },
+  separator: {
+    borderBottomColor: 'black',
+    borderBottomWidth: 1,
+    margin:10,
+    marginTop:5
+  },
 });
 
 const SingleRepository = () => {
   let { repositoryID } = useParams();
   let { repository } = useRepository(repositoryID);
-  
+
   if (!repository)
     return null;
 
-  return (
-    <View>
-      <RepositoryItem repository={repository} />
-      <View style={styles.buttonContainer}>
-        <Button
-          title="Open in GitHub"
-          onPress={() => { Linking.openURL(repository.url);}}
-          color={theme.colors.primary}
-        />
+  const RepositoryHeader = () => {
+    return (
+      <View>
+        <RepositoryItem repository={repository} />
+        <View style={styles.buttonContainer}>
+          <Button
+            title="Open in GitHub"
+            onPress={() => { Linking.openURL(repository.url); }}
+            color={theme.colors.primary}
+          />
+        </View>
       </View>
-    </View>
+    );
+  };
+  const ItemSeparator = () => <View style={styles.separator} />;
+
+  return (
+    <FlatList
+      data={repository.reviews.edges}
+      ItemSeparatorComponent={ItemSeparator}
+      renderItem={({ item }) => <Review review={item.node} />}
+      keyExtractor={item => item.node.id}
+      ListHeaderComponent={() => <RepositoryHeader/>}
+    />
   );
 };
 
